fed1b14ea392a949e4e6ebb2016410f9ea92a7a2
knaresh10 <20P31A0594@acet.ac.in>
2023-11-15 06:30:33 +0530

    added tokens

--- routes/user.js
+++ routes/user.js
@@ -13,11 +13,15 @@ router.get('/signup', (req, res) => {
 
 router.post('/signin', async (req, res) => {
     const { email, password } = req.body
-    const user = await User.matchPassword(email, password)
+    try {
+        const token = await User.matchPasswordAndGenerateToken(email, password)
+        return res.cookie('token', token).redirect('/')
+    } catch(e) {
+        res.render('signin', {
+            error : "Incorrect email or password"
+        })
+    }
 
-    console.log('User', user)
-
-    return res.redirect('/')
 })
 router.post('/signup',async (req, res) => {
     const { fullName, email, password } = req.body
@@ -27,6 +31,10 @@ router.post('/signup',async (req, res) => {
         password,
     })
     return res.redirect('/')
+})
+
+router.get('/logout', (req, res) => {
+    res.clearCookie('token').redirect('/')
 })
 
 module.exports = router
